9715b045e9f6498ca9c3542743effffee3a09808
Eshwar Sai Ram Mamillapalli <58381236+MamillaplliEshwarSaiRam@users.noreply.github.com>
2022-08-04 09:47:00 -0400

    Update scan.sh

--- scan.sh
+++ scan.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
-ips=$(sudo cat /var/log/auth.log | grep "Invalid user" | awk '{print $10}')
+ips=($(sudo cat /var/log/auth.log | grep "Invalid user" | awk '{print $10}'))
 dates1=($(sudo cat /var/log/auth.log | grep "Invalid user" | awk '{print $1}'))
 dates=($(sudo cat /var/log/auth.log | grep "Invalid user" | awk '{print $2}')) 
 
 all_ips=()
 for ip in $ips; do
-        all_ips+=($ip)
+        all_ips+=${ip}
 done
 
 all_dates1=()
 for date in ${dates1}; do
-        all_dates1=${date}
-        ech ${date}
+        all_dates1+=${date}
 done
 
 all_dates=()
@@ -21,11 +20,16 @@ done
 
 #echo "${all_dates[*]}"
 countries={}
-for ip in $dates; do
-        country=$(whois $(curl ifconfig.me) | grep -iE ^country | awk '{print $2}')
+for ip in $ips; do
+        country=$(curl ipinfo.io/$ip/country)
         countries+=${country}
+        echo ${country}
 done
 
 for ip in ${!countries[*]}; do
-        echo "${all_ips[ip]} ${countries[ip]} ${all_dates1[ip]} ${all_dates[ip]}" > /var/webserver_log/unauthorized.log
+        echo "${all_ips[$ip]}"
+        echo "${countries[$ip]}"
+        echo "${all_dates1[$ip]}"
+        echo "${all_dates[$ip]}"
+        echo "${all_ips[$ip]} ${countries[$ip]} ${all_dates1[$ip]} ${all_dates[$ip]}" > /var/webserver_log/unauthorized.log
 done
