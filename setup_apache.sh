4b34693cdf0b99fd05d415d9fcec94fe96ee6578
Eshwar Sai Ram Mamillapalli <58381236+MamillaplliEshwarSaiRam@users.noreply.github.com>
2022-08-05 23:00:39 -0400

    Update setup_apache.sh

--- setup_apache.sh
+++ setup_apache.sh
@@ -8,3 +8,4 @@ sudo chown nobody:nogroup /var/webserver_log
 echo "/var/webserver_log 192.168.1.2(rw,sync,no_root_squash,no_subtree_check)" |sudo tee -a /etc/exports
 sudo systemctl restart nfs-kernel-server
 sudo apt install whois
+sudo chmod -x scan.sh
