00f3e04e82d475022932825ac73c696edd4fd181
Eshwar Sai Ram Mamillapalli <58381236+MamillaplliEshwarSaiRam@users.noreply.github.com>
2022-08-07 18:54:33 -0400

    Update scan.sh

--- scan.sh
+++ scan.sh
@@ -1,28 +1,26 @@
 #!/bin/bash
 sudo apt-get install jq -y
 ips=$(sudo cat /var/log/auth.log | grep "Disconnected" | awk '{print $10}')
-dates1=$(sudo cat /var/log/auth.log | grep "Disconnected" | awk '{print $1}')
-dates=$(sudo cat /var/log/auth.log | grep "Disconnected" | awk '{print $2}')
+months=$(sudo cat /var/log/auth.log | grep "Disconnected" | awk '{print $1}')
+days=$(sudo cat /var/log/auth.log | grep "Disconnected" | awk '{print $2}')
 
 all_ips=()
 for ip in $ips; do
         all_ips+=($ip)
 done
-
-all_dates1=()
-for date in ${dates1}; do
-        all_dates1+=($date)
+all_datesm=()
+for date in $dates1; do
+        all_datesm+=($date)
 done
-
-all_dates=()
-for date in ${dates}; do
-        all_dates+=($date)
+all_datesd=()
+for date in $dates; do
+        all_datesd+=($date)
 done
-countries={}
+countries=()
 for ip in $ips; do
         country=$(curl -s ipinfo.io/${ip} | jq ".country")
         countries+=($country)
 done
 for ip in ${!countries[*]}; do
-        echo "${all_ips[ip]} ${countries[ip]} ${all_dates1[ip]} ${all_dates[ip]}" >>  /var/webserver_log/unauthorized.log
+    echo "${all_[ip]}  ${countries[ip]}  ${all_datesm[ip]}  ${all_datesd[ip]}" >> /var/webserver_log/unauthorized.log
 done
