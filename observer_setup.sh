1631028082b85c5adb92c85954328d82b87a96f9
Eshwar Sai Ram Mamillapalli <58381236+MamillaplliEshwarSaiRam@users.noreply.github.com>
2022-08-07 01:23:28 -0400

    Update observer_setup.sh

--- observer_setup.sh
+++ observer_setup.sh
@@ -3,5 +3,7 @@ set -x
 sudo apt update
 sudo apt install -y nfs-common
 sudo mkdir /var/webserver_monitor
+sudo chmod ugo+rwx /users/eshwar/monitor.sh
+sudo chmod ugo+rwx /var/webserver_monitor/unauthorized.log
 sudo mount 192.168.1.1:/var/webserver_log/ /var/webserver_monitor/
 sudo chmod -x /local/repository/monitor.sh
