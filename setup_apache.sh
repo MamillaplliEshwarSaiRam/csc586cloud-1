285b5ae5765fb042af078a10b3a5941465b3b86d
Eshwar Sai Ram Mamillapalli <58381236+MamillaplliEshwarSaiRam@users.noreply.github.com>
2022-08-07 15:58:56 -0400

    Update setup_apache.sh

--- setup_apache.sh
+++ setup_apache.sh
@@ -6,7 +6,6 @@ sudo apt install -y nfs-kernel-server
 sudo mkdir /var/webserver_log
 sudo chown nobody:nogroup /var/webserver_log
 echo "/var/webserver_log 192.168.1.2(rw,sync,no_root_squash,no_subtree_check)" |sudo tee -a /etc/exports
-sudo chmod -x /local/repository/scan.sh
 sudo chmod ugo+rwx /users/eshwar/scan.sh
 sudo chmod ugo+rwx /var/webserver_log/unauthorized.log
 sudo systemctl restart nfs-kernel-server
