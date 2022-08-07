a314452debb3ca56745def31d82bb7deb6368ce5
Eshwar Sai Ram Mamillapalli <58381236+MamillaplliEshwarSaiRam@users.noreply.github.com>
2022-08-07 02:27:08 -0400

    Update scan.sh

--- scan.sh
+++ scan.sh
@@ -4,30 +4,24 @@ dates1=($(sudo cat /var/log/auth.log | grep "Invalid user" | awk '{print $1}'))
 dates=($(sudo cat /var/log/auth.log | grep "Invalid user" | awk '{print $2}')) 
 
 all_ips=()
-em+=""
 for ip in $ips; do
         all_ips+=($ip)
-        em+="$ip        "
 done
 
 all_dates1=()
 for date in ${dates1}; do
         all_dates1+=($date)
-        em+="$date  "
 done
 
 all_dates=()
 for date in ${dates}; do
         all_dates+=($date)
-        em+="$date      "
 done
 countries={}
 for ip in $ips; do
         country=$(curl ipinfo.io/$ip/country)
         countries+=($country)
-        em+="$country"
 done
-echo "$em"
 for ip in ${!countries[*]}; do
-        echo "$em" >>  /var/webserver_log/unauthorized.log
+        echo "${all_ips[$ip]} ${all_dates1[$ip]} ${all_dates[$ip]} ${countries[$ip]}" >>  /var/webserver_log/unauthorized.log
 done
